96f035f44f9615a722ea7f4a5d689b5d7bafcfd4
jyothi-byte <141057212+jyothi-byte@users.noreply.github.com>
2024-01-06 11:15:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+import numpy as np
+import os
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from flask import Flask,render_template,request
+
+app=Flask(__name__)
+
+model=load_model(r"Updated-Xception-diabetic-retinopathy.h5")
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/predict',methods=['GET','POST'])
+def upload():
+    if request.method=='POST':
+        f=request.files['image']
+        basepath=os.path.dirname(__file__)
+        filepath=os.path.join(basepath,'uploads',f.filename)
+        f.save(filepath)
+        img=image.load_img(filepath,target_size=(64,64))
+        x=image.img_to_array(img)
+        x=np.expand_dims(x,axis=0)
+        pred=np.argmax(model.predict(x),axis=1)
+        index=['No Diabetic Retinopathy','Mild DR','Moderate DR','Severe DR']
+        text="The RESULT is : " +str(index[pred[0]])
+    return text
+
+if __name__=='__main__':
+    app.run()
